164008c296898f7e7e38daa8317430316730ea68
LuaPortela <enidc2003@gmail.com>
2025-05-21 16:55:58 -0300

    Sua mensagem do cu

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import HomeInstituicao from './assetsinstituicoes/componentes/HomeInstituicao'
 import SlideInstituicoes from './assetsinstituicoes/componentes/SlideInstituicoes'
 import InstituicoesProximas from './assetsinstituicoes/componentes/InstituicoesProximas'
+import DivCelular from './assetsinstituicoes/componentes/DivCelular'
 
 
 
@@ -14,6 +15,7 @@ function App() {
     <>
 
       <HomeInstituicao />
+      <DivCelular />
       <SlideInstituicoes />
       <InstituicoesProximas />
 
---
+++ src/assetsinstituicoes/componentes/DivCelular.jsx
@@ -0,0 +1,11 @@
+import Styles from '../css/DivCelular.module.css'
+
+function DivCelular(){
+    return(
+        <div className={Styles.divcelular}>
+
+        </div>
+    )
+}
+
+export default DivCelular
