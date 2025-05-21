78f6749124bf4f5f2a59b8bfa9abcce442905f33
LuaPortela <enidc2003@gmail.com>
2025-05-21 17:47:30 -0300

    atualizado

--- src/assetsinstituicoes/componentes/DivCelular.jsx
+++ src/assetsinstituicoes/componentes/DivCelular.jsx
@@ -12,7 +12,7 @@ function DivCelular(){
 
         <div className={Styles.divcelular2}>
             <img src={iconInstitui2} alt="Ícone de instituição" className={Styles.celularIconeEstatistica2} />
-            <p>Mais de 3.000 crianças são atendidas por instituições parceiras em todo o Brasil.</p>
+            <p>Mais de 3.000 crianças são atendidas por instituições.</p>
         </div>
     </main>
                    
