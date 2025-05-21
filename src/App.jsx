ab97555ce73c4891fc2e8bb68ff015168bf06a4f
LuaPortela <enidc2003@gmail.com>
2025-05-21 16:05:15 -0300

    descrição do commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
-import HomeInstituicao from './assets_instituicoes/componentes/HomeInstituicao'
-import InstituicoesProximas from './assets_instituicoes/componentes/Instituicoesproximas'
-import SlideInstituicoes from './assets_instituicoes/componentes/SlideInstituicoes'
+import HomeInstituicao from './assetsinstituicoes/componentes/HomeInstituicao'
+import InstituicoesProximas from './assetsinstituicoes/componentes/Instituicoesproximas'
+import SlideInstituicoes from './assetsinstituicoes/componentes/SlideInstituicoes'
 
 
 
